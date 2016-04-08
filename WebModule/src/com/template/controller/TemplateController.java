aafedf315caca2112346dfc2f398657c6b03d4f6
focusardi <focusardi@gmail.com>
2016-04-08 15:47:43 +0800

    spring mvc

--- WebModule/src/com/template/controller/TemplateController.java
+++ WebModule/src/com/template/controller/TemplateController.java
@@ -1,5 +1,19 @@
 package com.template.controller;
 
-public class TemplateController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class TemplateController {
+	
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+		System.out.println("123333");
+		
+		
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("template/welcome", "message", message);
+	}
 }
