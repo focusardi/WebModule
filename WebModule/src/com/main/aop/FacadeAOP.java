a382a8a7e6e3afac3aa3a252cdf098451d22c65e
focusardi <focusardi@gmail.com>
2016-04-15 10:08:24 +0800

    facade aop

--- WebModule/src/com/main/aop/FacadeAOP.java
+++ WebModule/src/com/main/aop/FacadeAOP.java
@@ -6,19 +6,18 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
-public class ControllerAOP {
+public class FacadeAOP {
 	
-	@Pointcut("execution(* com.*.controller.*.*(..))")
+	@Pointcut("execution(* com.*.facade.*.*(..))")
 	public void pointcut() {
 		// 定義一個pointcut，下面用Annotation標注的通知方法可以公用這個pointcut
 	}
 	
 	@Around("pointcut()")
 	public Object myAroundAdvice(ProceedingJoinPoint jionpoint) throws Throwable {
-		System.out.println("Controller AoP================================================BEGIN");		
+		System.out.println("Facade AoP================================================BEGIN");		
 		Object o = jionpoint.proceed();
-		System.out.println("Controller AoP================================================END");	
-		
+		System.out.println("Facade AoP================================================END");			
 		return o;
 	}
 	
